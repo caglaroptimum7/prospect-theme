4880f4a388e075d5d364673396af8a70968f231b
caglarptimum7 <caglar@optimum7.com>
2020-05-04 13:55:28 +0300

    netlify deploy

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,34 @@
 'use strict';
-const gulp = require('gulp');
+const { watch, series, parallel, src, dest, gulp } = require('gulp');
 const sass = require('gulp-sass');
 const browserSync = require('browser-sync').create();
 const minify = require('gulp-minify');
+const connect = require('gulp-connect'); // Runs a local webserver
+const open = require('gulp-open'); // Opens a URL in a web browser
+const exec = require('child_process').exec; // run command-line programs from gulp
+const execSync = require('child_process').execSync; // command-line reports
 
+// Launch Chrome web browser
+// https://www.npmjs.com/package/gulp-open
+function openBrowser(done) {
+    var options = {
+    uri: 'http://localhost:8080'
+    };
+    return src('./app')
+    .pipe(open(options));
+    done();
+}
 
+// Gulp plugin to run a webserver (with LiveReload)
+// https://www.npmjs.com/package/gulp-connect
+function server(done) {
+    return connect.server({
+    root: './app',
+    port: 8080,
+    debug: true,
+    });
+    done();
+}
 
 //compile scss into css
 function style() {
@@ -23,24 +47,38 @@ function scripts() {
         .pipe(browserSync.stream());
 }
 
-
-
-function watch() {
-    browserSync.init({
-        server: {
-            baseDir: "./app",
-            index: "/index.html"
-        }
-    });
-    gulp.watch('./app/assets/scss/*.scss', style);
-    gulp.watch('./app/assets/js/app.js', scripts);
-    gulp.watch('./app/*.html').on('change',browserSync.reload);
-    gulp.watch('./app/pages/*.html').on('change',browserSync.reload);
-    gulp.watch('./app/assets/js/*.js').on('change', browserSync.reload);
+// Commit and push files to Git
+function git(done) {
+    return exec('git add . && git commit -m "netlify deploy" && git push');
+    done();
 }
 
-exports.style = style;
-exports.watch = watch;
-exports.scripts = scripts;
-exports.default = gulp.series(watch);
+// Watch for netlify deployment
+function netlify(done) {
+    return new Promise(function(resolve, reject) {
+        console.log(execSync('netlify watch').toString());
+        resolve();
+    });
+}
+
+// Preview Deployment
+function netlifyOpen(done) {
+    return exec('netlify open:site');
+    done();
+}
+
+function watchFiles() {
+    watch('./app/assets/scss/*.scss', style);
+    watch('./app/assets/js/app.js', scripts);
+    watch('./app/*.html').on('change',browserSync.reload);
+    watch('./app/pages/*.html').on('change',browserSync.reload);
+    watch('./app/assets/js/*.js').on('change', browserSync.reload);
+}
+
+
+exports.deploy = series(git, netlify, netlifyOpen);
+
+// Default Gulp command
+
+exports.default = series(openBrowser, server, watchFiles);
 
